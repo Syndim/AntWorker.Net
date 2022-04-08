90a1e825db2532b88b4e8cbc084052d71ee67a78
Syndim <im@syndim.org>
2022-04-08 16:28:50 +0800

    Add exit code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AntWorker.Net
 {
     internal static class CommandArgs
     {
-        public static async Task InvokeAsync(string[] args)
+        public static async Task<int> InvokeAsync(string[] args)
         {
             var commands = new List<ICommandProperty>
             {
@@ -16,15 +16,15 @@ namespace AntWorker.Net
 
             var rootCommand = new RootCommand();
             commands.ForEach(c => c.AddToCommand(rootCommand));
-            await rootCommand.InvokeAsync(args);
+            return await rootCommand.InvokeAsync(args);
         }
     }
 
     class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            await CommandArgs.InvokeAsync(args);
+            return await CommandArgs.InvokeAsync(args);
         }
     }
 }
